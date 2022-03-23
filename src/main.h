6f82685bda584dbdaa2db6abb7a14550458888ea
Brandon <evilsupergenius4@gmail.com>
2022-03-22 21:23:49 -0400

    GPS working but needs testing

---
+++ src/main.h
@@ -0,0 +1,19 @@
+//https://www.tutorialspoint.com/cprogramming/c_header_files.htm
+//a practice in C or C++ is we keep all the constants, macros, system wide global variables, and function prototypes in the header files and include that header file.
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+//This function notifies the user of the error number by blink/beep and prints error code to serial, use this to look up what error numbers mean
+void errorNotify(byte, bool);
+
+//@TODO-handle prototype functions better
+void run(int);
+void brake(int);
+void waitForGPSFix(void);
+void encodeGPSSerial(void);
+
+void testGPSSim(void);
+
+boolean saveToSD(String);
+
+#endif
